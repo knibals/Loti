76ac6373db5b52744d1a39bb7b78371c8c7b2d3d
Oumar FALL <oumar.fall@orange-sonatel.com>
2019-11-26 12:13:31 +0000

    Reboot the theme (Bootstrap 4+ version)

---
+++ js/loti.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Loti behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.loti = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
