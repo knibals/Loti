cd628dd36738ee4f60b095da4583f38271dc4af0
Oumar FALL <oumar.fall@orange-sonatel.com>
2019-11-27 14:53:02 +0000

    load the necessary files to build a slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,12 @@
+(function ($, Drupal) {
+
+    'use strict';
+  
+    Drupal.behaviors.slideshow = {
+      attach: function (context, settings) {
+        console.log('slideshow script loaded!');
+      }
+    };
+  
+  } (jQuery, Drupal));
+  
